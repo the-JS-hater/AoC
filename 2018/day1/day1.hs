cb196df5e0fe13c23d249e1a142d1b295beeadca
Morgan <morno368@student.liu.se>
2024-10-07 11:51:18 +0200

    some Haskell practice

---
+++ 2018/day1/day1.hs
@@ -0,0 +1,10 @@
+main :: IO ()
+main = do
+  contents <- readFile "input.txt"
+  let intList = map read (lines contents) :: [Integer]
+  let result = solve 0 intList
+  print result
+
+solve :: Integer -> [Integer] -> Integer
+solve n [] = n
+solve n (x : xs) = solve (n + x) xs
